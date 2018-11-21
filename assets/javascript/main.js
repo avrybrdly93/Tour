52fad38d06791d90f4a495b8371842d65a3ef93e
Avery <cbra067@lasierra.edu>
2018-11-20 21:41:08 -0800

    First commit on new branch

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,36 +1,63 @@
 $(document).ready(function() {
 
-// variable definitions
-let userInputDiv = $("#search");
-let userInput = "";
-let userButtonInput = "";
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyDqJ3VgayN_s8weHtA_Ezr8CdXx96aRw4w",
-    authDomain: "tour-f4cd2.firebaseapp.com",
-    databaseURL: "https://tour-f4cd2.firebaseio.com",
-    projectId: "tour-f4cd2",
-    storageBucket: "",
-    messagingSenderId: "1019598312395"
-};
-firebase.initializeApp(config);
-
-database = firebase.database();
-
-database.ref().set({
-    testData: "hello"
-})
-// User input 
-$("#search-button").on("click", function(event) {
-    event.preventDefault();
-    userInput = userInputDiv.val();
-    console.log(userInput);
-});
-
-$(".user-button").on("click", function() {
-    userButtonInput = $(this).attr("data-value");
-    console.log(userButtonInput);
-})
-
-
-});
+    // variable definitions
+    let userInputDiv = $("#search");
+    let userInput = "";
+    let userButtonInput = "";
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyDqJ3VgayN_s8weHtA_Ezr8CdXx96aRw4w",
+        authDomain: "tour-f4cd2.firebaseapp.com",
+        databaseURL: "https://tour-f4cd2.firebaseio.com",
+        projectId: "tour-f4cd2",
+        storageBucket: "",
+        messagingSenderId: "1019598312395"
+    };
+    firebase.initializeApp(config);
+    
+    database = firebase.database();
+    
+    function pushToFirebase() {
+        database.ref().set({
+            testData: "hello"
+        });
+    }
+    function readFromFirebase() {
+        database.ref().on("child_added", function(snapshot) {
+            sv = snapshot.val();
+            console.log(sv);
+    }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+    });
+    }
+    function search(event) {
+        event.preventDefault();
+        userInput = userInputDiv.val();
+        console.log(userInput);
+    }
+    function searchButton() {
+        userButtonInput = $(this).attr("data-value");
+        console.log(userButtonInput);
+    }
+    function ajaxCall(APIURL, APIKey) {
+        let queryURL = APIURL + userInput + "&api_key=" + APIKey;
+        console.log(userInput);
+        console.log(queryURL);
+        $.ajax({
+            url:queryURL,
+            method: "GET"
+        }).then(function(response){
+            console.log(response.data);
+        })
+    }
+    // User input 
+    $("#search-button").on("click", function(event) {
+        event.preventDefault();
+        userInput = userInputDiv.val();
+        console.log(userInput);
+    });
+    
+    $(".user-button").on("click", searchButton);
+    
+    
+    });
