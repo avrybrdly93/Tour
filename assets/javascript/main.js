d4514874fa29fae8c45fc705ba090637948a8da1
Dolores Hernandez <dolores.e.hernandez@gmail.com>
2018-11-26 21:29:05 -0800

    Search button fixed

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,42 +1,37 @@
 $(document).ready(function() {
-
-// User input 
-$("#search-button").on("click", function(event) {
+  // User input
+  $("#main-search-btn").on("click", function() {
     event.preventDefault();
-    userInput = userInputDiv.val();
-    console.log(userInput);
-});
+    console.log("Button Clicked!");
+    let userInput = $("#search")
+      .val()
+      .trim();
+  });
 
-$(".user-button").on("click", function() {
+  $(".user-button").on("click", function() {
     userButtonInput = $(this).attr("data-value");
     console.log(userButtonInput);
     // awdk;jf;asdljf;lasdjfasd;flj
     //asdfasdfasdfasdfasdfa
     //asdfasdfasdfasdf
     //asdfasdfasdfasdf
-})
+  });
 
+  var state = $("#opt").attr("data-state");
 
-
-    
-var state = $("#opt").attr('data-state');
-
-if (state === 'hide'){
-    $("#opt").click(function(){
-        $(".text").hide();
-        console.log('hide');
-        $("#opt").attr('state', 'show');
+  if (state === "hide") {
+    $("#opt").click(function() {
+      $(".text").hide();
+      console.log("hide");
+      $("#opt").attr("state", "show");
     });
-}
-if (state === 'show'){
-    $("#opt").click(function(){
-        $(".text").show();
-        console.log('show');
-        $("#opt").attr((this, 'hide'));
-        console.log($("#opt").attr('data-state'));
+  }
+  if (state === "show") {
+    $("#opt").click(function() {
+      $(".text").show();
+      console.log("show");
+      $("#opt").attr((this, "hide"));
+      console.log($("#opt").attr("data-state"));
     });
-}
-
-
-
+  }
 });
